cd8ad47ee4e5beea71b82fe4640f297664617bc4
Oliver Philbrick <oliveman521@gmail.com>
2024-03-02 22:40:13 -0500

    Added phase randomization to Jester Movement + level 1-3b

--- Enemies/Enemy_SineMovement.gd
+++ Enemies/Enemy_SineMovement.gd
@@ -3,13 +3,16 @@ extends Enemy
 
 @export var amplitude: float = 150
 @export var period: float = 500
+@export var randomize_phase: bool = true
 var starting_pos: Vector2
-
+var phase_shift: float = 0
 func activate() -> void:
 	super.activate()
 	starting_pos = global_position
+	if randomize_phase:
+		phase_shift = randf_range(0,period)
 
 func move(delta: float) -> void:
 	super.move(delta)
-	position.y = starting_pos.y + amplitude*sin(position.x *2* PI /period)
+	position.y = starting_pos.y + amplitude*sin((position.x *2* PI /period) + phase_shift)
 
